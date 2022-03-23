c803699d028b29bb91a5c920f0daf1927bca62b9
Burçak <89997401+burcakkok@users.noreply.github.com>
2022-03-23 20:53:20 +0100

    class exercises done

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,26 @@
-alert("hello!");
 
-document.querySelector('#new-todo').addEventListener('submit', (e) => {
-    e.preventDefault();
-    const text = e.target.elements.text.value.trim();
-    alert(text);
-})
+
+const createButton = document.querySelector('#create-button');
+
+let index = 0;
+const clickEvent = (event) => {
+    const colors = ['tomato', 'blue', 'pink'];
+    event.preventDefault();
+    console.log("Someone just clicked me once!");
+    createButton.style.backgroundColor = colors[index];
+    index++;
+}
+
+createButton.addEventListener("click", clickEvent);
+
+createButton.addEventListener("dblclick", (event) => {
+    event.preventDefault();
+    console.log("Someone just clicked me twice!");
+});
+
+const inputSelect = document.querySelector('#search-text');
+
+inputSelect.addEventListener('input', (event) => {
+    event.preventDefault();
+    console.log(event.target.value);
+});
